acd1ba0b2a8536dd3616dc8c662dd08b97cdffeb
baer1 <videocreator@outlook.de>
2026-09-02 01:54:49 +0200

    Improve build script output
    logging change, no need to rebuild v2.5.2

--- prebuild.sh
+++ prebuild.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+echo "Creating lib & temporary directories..."
 mkdir -p lib
-
 temp="$(mktemp -d)"
 
+echo "Downloading and extracting LobbyControl..."
 wget -O "${temp}/LobbyControl.zip" https://thunderstore.io/package/download/mattymatty/LobbyControl/2.5.12/
 7z e -aoa "${temp}/LobbyControl.zip" -olib "BepInEx/plugins/LobbyControl.dll"
